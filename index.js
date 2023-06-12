31d058f0d3f2396084f6b69ececf70c00af038e9
eldarmmm <eldarmamratov@gmail.com>
2023-06-12 20:02:04 +0600

    check

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const session = require('express-session')
+const mongooseStore = require('connect-mongo')
+const passport = require('passport')
+
+const app = express();
+
+require('./server/config/db')
+require('./server/config/passport')
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded())
+app.use(session({
+        name: 'decode.session',
+        secret: 'keyboard cat',
+        maxAge: 1000 * 60 * 60 * 7,
+        resave: false,
+        store: mongooseStore.create({
+            mongoUrl: 'mongodb://127.0.0.1:27017'
+        })
+})) 
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.set('view engine' , 'ejs')
+app.use(require('./server/auth/router'))
+app.use(require('./server/pages/router'))
+app.use(require('./server/Blogs/router'))
+app.use(require('./server/Posts/router'))
+
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+})
+
